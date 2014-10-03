3b9926ce170e58351aed9b29d7fcc06bb8d3cb25
xiang xiao <btxiaox@gmail.com>
2014-10-04 01:54:54 +0800

    sqls to create tables

---
+++ WebContent/WEB-INF/SQL/MESSAGE.sql
@@ -0,0 +1,39 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : Postgresql
+ Source Server Type    : PostgreSQL
+ Source Server Version : 90305
+ Source Host           : localhost
+ Source Database       : Interview
+ Source Schema         : Interview
+
+ Target Server Type    : PostgreSQL
+ Target Server Version : 90305
+ File Encoding         : utf-8
+
+ Date: 10/04/2014 00:16:05 AM
+*/
+
+-- ----------------------------
+--  Table structure for MESSAGE
+-- ----------------------------
+DROP TABLE IF EXISTS interview.MESSAGE;
+CREATE TABLE interview.MESSAGE (
+	MESSAGE_ID serial,
+	REPLY_MESSAGE_ID int4 NOT NULL,
+	USER_ID int4 NOT NULL,
+	USER_NICK_NAME varchar NOT NULL,
+	MESSAGE_CONTENT varchar(100) NOT NULL,
+	TIME_STAMP timestamp(6) NOT NULL
+)
+WITH (OIDS=FALSE);
+ALTER TABLE Interview.MESSAGE OWNER TO postgres;
+
+COMMENT ON COLUMN Interview.MESSAGE.USER_NICK_NAME IS 'duplicate record';
+
+-- ----------------------------
+--  Primary key structure for table MESSAGE
+-- ----------------------------
+ALTER TABLE Interview.MESSAGE ADD PRIMARY KEY (MESSAGE_ID) NOT DEFERRABLE INITIALLY IMMEDIATE;
+
---
+++ WebContent/WEB-INF/SQL/USER.sql
@@ -0,0 +1,39 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : Postgresql
+ Source Server Type    : PostgreSQL
+ Source Server Version : 90305
+ Source Host           : localhost
+ Source Database       : Interview
+ Source Schema         : Interview
+
+ Target Server Type    : PostgreSQL
+ Target Server Version : 90305
+ File Encoding         : utf-8
+
+ Date: 10/04/2014 00:24:29 AM
+*/
+
+-- ----------------------------
+--  Table structure for USER
+-- ----------------------------
+DROP TABLE IF EXISTS interview.USERS;
+CREATE TABLE interview.USERS (
+	USER_ID serial,
+	NICK_NAME varchar(50),
+	TOKEN varchar(20) ,
+	USER_TYPE varchar(20) 
+)
+WITH (OIDS=FALSE);
+ALTER TABLE Interview.USERS OWNER TO postgres;
+
+COMMENT ON COLUMN Interview.USERS.USER_ID IS 'UserId';
+COMMENT ON COLUMN Interview.USERS.NICK_NAME IS 'NICK_NAME';
+COMMENT ON COLUMN Interview.USERS.TOKEN IS 'DYNAMIC TOKEN';
+
+-- ----------------------------
+--  Primary key structure for table USER
+-- ----------------------------
+ALTER TABLE Interview.USERS ADD PRIMARY KEY (USER_ID) NOT DEFERRABLE INITIALLY IMMEDIATE;
+
